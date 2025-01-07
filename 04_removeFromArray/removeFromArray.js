42a1dd80841694dc50e947f08811b6d25fe9cde2
Rafael Pimentel <47717581+gustanini@users.noreply.github.com>
2025-01-07 11:05:44 +0100

    feat: solve ex 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...args) { // get arbitrary number of args
+    let outputArray = [];                               // empty output array
+    // function expression to iterate each element
+    inputArray.forEach(element => {
+        if (!args.includes(element)) {                  // check if element is not in args
+            outputArray.push(element);
+        }
+    });
+    return outputArray;
 };
 
 // Do not edit below this line
