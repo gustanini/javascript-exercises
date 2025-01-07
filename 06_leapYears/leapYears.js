4c210351180f9a3c1933953b60ee1236058db505
Rafael Pimentel <47717581+gustanini@users.noreply.github.com>
2025-01-07 11:37:06 +0100

    feat: solve ex 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 !== 0) return false;                       // not divisible by 4
+    if (year % 100 == 0 && year % 400 !== 0) return false;  // divisible by 100 but not by 400
+    return true;                                            // leap true
 };
 
 // Do not edit below this line
