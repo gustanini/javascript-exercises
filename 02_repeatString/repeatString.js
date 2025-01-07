3376e535fbaf7f8812731e2b7f98071e20d52409
Rafael Pimentel <47717581+gustanini@users.noreply.github.com>
2025-01-07 09:14:29 +0100

    feat: solved problem 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, times) {
+    // validate times is more than 0
+    if (times < 0) return "ERROR";
+    // create empty output string
+    let outputString = "";                                  
+    // iterate 
+    for (i = 0; i < times; i++) {
+        outputString += inputString;                        // concatenate inStr to outStr
+        //outputString = outputString.concat(inputString);    // concatenate inStr to outStr
+    }
+    return outputString;
 };
 
 // Do not edit below this line
