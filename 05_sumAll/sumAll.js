8fb224e7fc2db68ffb4f2a195408573d79d88547
Rafael Pimentel <47717581+gustanini@users.noreply.github.com>
2025-01-07 11:22:32 +0100

    solve ex 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(numberOne, numberTwo) {
+    // validate numbers are positive ints
+    if (!Number.isInteger(numberOne) || !Number.isInteger(numberTwo)) return 'ERROR';
+    if (numberOne < 0 || numberTwo < 0) return 'ERROR';
+    
+    // calculate sum of all digits
+    let sum = number => {
+        let result = 0;
+        for (i = 0; i = number; i++){
+            result += i;
+        }
+    }
+    
+    return sum(numberOne) + sum(numberTwo);
 };
 
 // Do not edit below this line
