65ad28c8681c3348b5ff68bcc105745d38f2c069
Rafael Pimentel <47717581+gustanini@users.noreply.github.com>
2025-01-07 10:45:44 +0100

    feat: solve ex 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+    reversedString = "";                        // empty reversedString variable (not the same as reverseString function)
+    inputStringLenght = inputString.length;     // get length
 
+    for (i = 0; i < inputStringLenght; i++) {
+        // iterate through each character in reverse, populating reversedString
+        reversedString += inputString[inputStringLenght - 1 - i];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
